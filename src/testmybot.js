131aaf4eca45837bfeb3f0e2b315776e928c4855
Astam Aliyev <astam@letslinc.com>
2018-03-05 14:51:14 -0800

    added recipient

--- src/testmybot.js
+++ src/testmybot.js
@@ -101,10 +101,10 @@ module.exports = class TestMyBot {
     })
   }
 
-  hears (arg, sender) {
+  hears (arg, sender, recipient) {
     if (this.container) {
       if (_.isString(arg)) {
-        return this.container.UserSaysText(arg, sender)
+        return this.container.UserSaysText(arg, sender, recipient)
       } else {
         return this.container.UserSays(arg)
       }
